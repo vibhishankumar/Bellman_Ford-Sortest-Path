312a159189717608d12fbb1c5cee6517cfdd595e
vibhishan kumar <47953033+vibhishankumar@users.noreply.github.com>
2020-10-18 13:14:32 +0530

    Add files via upload
    Bellman -Ford algorithm implementation for negative weighted edge in c++

--- bellman.cpp
+++ bellman.cpp
@@ -1,13 +1,11 @@
-// A C++ program for Bellman-Ford's single source 
-// shortest path algorithm. 
-#include <bits/stdc++.h> 
-
+ #include <bits/stdc++.h> 
+  using namespace std;
 // a structure to represent a weighted edge in graph 
 struct Edge { 
 	int src, dest, weight; 
 }; 
 
-// a structure to represent a connected, directed and 
+// Graph to represent a connected, directed and 
 // weighted graph 
 struct Graph { 
 	// V-> Number of vertices, E-> Number of edges 
@@ -28,29 +26,27 @@ struct Graph* createGraph(int V, int E)
 } 
 
 // A utility function used to print the solution 
-void printArr(int dist[], int n) 
+void displaySol(int dist[], int n) 
 { 
-	printf("Vertex Distance from Source\n"); 
+	cout<<"Vertex Distance from Source"<<endl; 
 	for (int i = 0; i < n; ++i) 
-		printf("%d \t\t %d\n", i, dist[i]); 
+	     cout<<i<<"  "<<dist[i]<<endl;
+		//printf("%d \t\t %d\n", i, dist[i]); 
 } 
-
-// The main function that finds shortest distances from src to 
-// all other vertices using Bellman-Ford algorithm. The function 
+//  Bellman-Ford algorithm. The function 
 // also detects negative weight cycle 
-void BellmanFord(struct Graph* graph, int src) 
+void BellmanSol(struct Graph* graph, int src) 
 { 
 	int V = graph->V; 
 	int E = graph->E; 
 	int dist[V]; 
 
-	// Step 1: Initialize distances from src to all other vertices 
-	// as INFINITE 
+	// Initialize distances from src to all other vertices as INFINITE 
 	for (int i = 0; i < V; i++) 
 		dist[i] = INT_MAX; 
 	dist[src] = 0; 
 
-	// Step 2: Relax all edges |V| - 1 times. A simple shortest 
+	// Relax all edges |V| - 1 times. A simple shortest 
 	// path from src to any other vertex can have at-most |V| - 1 
 	// edges 
 	for (int i = 1; i <= V - 1; i++) { 
@@ -63,31 +59,28 @@ void BellmanFord(struct Graph* graph, int src)
 		} 
 	} 
 
-	// Step 3: check for negative-weight cycles. The above step 
-	// guarantees shortest distances if graph doesn't contain 
-	// negative weight cycle. If we get a shorter path, then there 
-	// is a cycle. 
+	// check for negative-weight cycle.
 	for (int i = 0; i < E; i++) { 
 		int u = graph->edge[i].src; 
 		int v = graph->edge[i].dest; 
 		int weight = graph->edge[i].weight; 
 		if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) { 
-			printf("Graph contains negative weight cycle"); 
+			cout<<"Graph contains negative weight cycle"; 
 			return; // If negative cycle is detected, simply return 
 		} 
 	} 
 
-	printArr(dist, V); 
+	displaySol(dist, V); 
 
 	return; 
 } 
-using namespace std;
-// Driver program to test above functions 
+
+//main  Driver program 
 int main() 
 { 
 	/* Let us create the graph given in above example */
-	/*int V = 5; // Number of vertices in graph 
-	int E = 8; // Number of edges in graph 
+	/*int V  // Number of vertices in graph 
+	int E  // Number of edges in graph 
     */
     int V,E;
     cout<<"enter Number of vertex and Edge in the grapgh"<<endl;
@@ -101,47 +94,7 @@ int main()
 	   cin>> graph->edge[i].weight; 
     }
 
-	// add edge 0-1 (or A-B in above figure) 
-	/*graph->edge[0].src = 0; 
-	graph->edge[0].dest = 1; 
-	graph->edge[0].weight = -1; 
-
-	// add edge 0-2 (or A-C in above figure) 
-	graph->edge[1].src = 0; 
-	graph->edge[1].dest = 2; 
-	graph->edge[1].weight = 4; 
-
-	// add edge 1-2 (or B-C in above figure) 
-	graph->edge[2].src = 1; 
-	graph->edge[2].dest = 2; 
-	graph->edge[2].weight = 3; 
-
-	// add edge 1-3 (or B-D in above figure) 
-	graph->edge[3].src = 1; 
-	graph->edge[3].dest = 3; 
-	graph->edge[3].weight = 2; 
-
-	// add edge 1-4 (or A-E in above figure) 
-	graph->edge[4].src = 1; 
-	graph->edge[4].dest = 4; 
-	graph->edge[4].weight = 2; 
-
-	// add edge 3-2 (or D-C in above figure) 
-	graph->edge[5].src = 3; 
-	graph->edge[5].dest = 2; 
-	graph->edge[5].weight = 5; 
-
-	// add edge 3-1 (or D-B in above figure) 
-	graph->edge[6].src = 3; 
-	graph->edge[6].dest = 1; 
-	graph->edge[6].weight = 1; 
-
-	// add edge 4-3 (or E-D in above figure) 
-	graph->edge[7].src = 4; 
-	graph->edge[7].dest = 3; 
-	graph->edge[7].weight = -3; */
-
-	BellmanFord(graph, 0); 
+		BellmanSol(graph, 0); 
 
 	return 0; 
 } 
